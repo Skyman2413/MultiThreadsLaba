2da35430eb438e9315168016f504a0a21211495f
skyman2413 <stepan26062001@gmail.com>
2023-04-04 00:08:39 +0300

    still working

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections.Concurrent;
 using ConsoleApp1;
 
+
+class Job
+{
+    public string OrgName { get; set; }
+    public long Price { get; set; }
+    public DateTime Date { get; set; }
+    public List<string>? ProductList { get; set; }
+    public string State { get; set; }
+    
+}
 class Program
 {
-    public static  BlockingCollection<Dictionary<string, string>> ServerQueue = new ();
+    public static BlockingCollection<Job> ServerQueue = new ();
     public static Dictionary<string, string> JobTemplate = new()
     {
         {"org_name", "sample_name"},
         {"count_price", "199234"},
         {"date", "12.04.2023"},
-        {"product_list", "productA, productB"}
+        {"product_list", "productA, productB"},
+        {"state", "in_progress"}
     };
     private static void worker()
     {
--- ConsoleApp1/Server_threadpool.cs
+++ ConsoleApp1/Server_threadpool.cs
@@ -1,7 +1,7 @@
-﻿
-using System.Net;
-using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 
 namespace ConsoleApp1;
 
@@ -11,7 +11,9 @@ public class Server
     public Server() {
         // Создаем листенер и конечные адреса для запросов
         _listener = new HttpListener();
+        // такой запрос должен быть POST (см REST API), т.к. он изменяет ресурс сервера
         _listener.Prefixes.Add("http://127.0.0.1:8080/new_item/");
+        // такой запрос должен быть GET (см REST API), т.к. он получает ресурс сервера
         _listener.Prefixes.Add("http://127.0.0.1:8080/current_state/");
 
     }
@@ -38,7 +40,96 @@ public class Server
     private void Process(object o) {
         var context = o as HttpListenerContext;
         // логика обработки запроса
+        var request = context.Request;
+        var endpoint = request.RawUrl;
+        var response = context.Response;
+        var encoding = response.ContentEncoding ?? Encoding.UTF8;
+        response.KeepAlive = false;
+        var responseData = "";
         
+        // так же есть более симпатичная реализация с маппингом, но она сложна для чтения и понимая,
+        // тут стараюсь использовать как можно более базовые вещи. Так писать не стоит
+        switch (endpoint)
+        {
+            case "/new_item/":
+            {
+                // для создания новой задачи
+                try
+                {
+                    CreateNewItem();
+                }
+                catch
+                {
+                    response.StatusCode = 400;
+                    response.StatusDescription = "Bad Request";
+                }
+
+                break;
+            }
+            case "/current_state/":
+            {
+                //TODO для получения данных об очереди
+                break;
+            }
+            default:
+            {
+                //TODO
+                break;
+            }
+        }
+
+        byte[] buffer = Encoding.UTF8.GetBytes(responseData);
+        // получаем поток ответа и пишем в него ответ
+        response.ContentLength64 = buffer.Length;
+        using (var output = response.OutputStream)
+        {
+            output.Write(buffer);
+        }
+        
+
+        void CreateNewItem()
+        {
+            // создание нового элемента очереди
+            if (request.HttpMethod.ToLower() != "post")
+            {
+                response.StatusCode = 405;
+                response.StatusDescription = "Method is not allowed";
+                return;
+            }
+
+            if (request.ContentType is not ("application/json" or "text/json"))
+            {
+                response.StatusCode = 415;
+                response.StatusDescription = "Unsupported Media Type";
+                return;
+            }
+
+            var job = GetDataFromRequest();
+            if (job is not null) Program.ServerQueue.Add(job);
+            else
+            {
+                response.StatusCode = 500;
+                response.StatusDescription = "Internal Server Error";
+            }
+        }
+
+        Job? GetDataFromRequest()
+        {
+            // преобразовываем инфо из запроса в Job-объект
+            var res = "";
+            using (var receiveStream = request.InputStream)
+            {
+                using (var readStream = new StreamReader(receiveStream, encoding))
+                {
+                    res = readStream.ReadToEnd();
+                }
+            }
+            
+            return JsonSerializer.Deserialize<Job>(res);
+        }
+            
+    
     }
+    
 
 }
