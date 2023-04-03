0f50e15495c78409ae9c536229b885fb05b51064
skyman2413 <stepan26062001@gmail.com>
2023-04-03 22:17:17 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using ConsoleApp1;
+
+class Program
+{
+    public static  BlockingCollection<Dictionary<string, string>> _serverQueue = new ();
+    public static Dictionary<string, string> _jobTemplate = new()
+    {
+        {"org_name", "sample_name"},
+        {"count_price", "199234"},
+        {"date", "12.04.2023"},
+        {"product_list", "productA, productB"}
+    };
+    private static void worker()
+    {
+
+    }
+
+    public static void Main(string[] args)
+    {
+        var serv = new Server();
+        var server = new Thread(serv.Start_working);
+        // TODO сделать тредпул для воркеров
+        const int threadsCount = 8;
+        Thread[] consumers = new Thread[threadsCount];
+
+        for (int i = 0; i < threadsCount; i++)
+        {
+            consumers[i] = new Thread(worker);
+            consumers[i].Start();
+        }
+
+        server.Start();
+        server.Join();
+        for (int i = 0; i < threadsCount; i++)
+        {
+            consumers[i].Join();
+        }
+    }
+}
+
+
---
+++ ConsoleApp1/Server.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Net.Sockets;
+
+namespace ConsoleApp1;
+
+public class Server
+{
+    
+    public void Start_working()
+    {
+        var tcpListener = new TcpListener()
+        try
+        {
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
